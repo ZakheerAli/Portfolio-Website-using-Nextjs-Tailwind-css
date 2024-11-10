d31cf3f1fed44ce7096688171e3385bf884e2237
Zakheer Ali <159714713+ZakheerAli@users.noreply.github.com>
2024-11-10 22:02:39 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <h1 className="text-7xl font-extrabold text-zinc-500
           mb-5">HI, MY NAME IS <span className="text-7xl font-extrabold text-orange-500">ZAKHEER ALI</span></h1>
           <h4 className="text-4xl font-bold  mt-5 text-orange-500">FRONT END DEVELOPER</h4>
-          <p className="text-lg font-semibold mt-5 mb-10 text-zinc-500 ">“With a passion for design and a focus on user-centered development, I build modern, responsive websites that look great and work smoothly on any device. Whether creating dynamic resumes, interactive portfolios, or complex web applications, I’m driven by a commitment to clean code and effective design.”</p>
+          <p className="text-lg font-semibold mt-5 mb-10 text-zinc-500 ">With a passion for design and a focus on user-centered development, I build modern, responsive websites that look great and work smoothly on any device. Whether creating dynamic resumes, interactive portfolios, or complex web applications, I am driven by a commitment to clean code and effective design.</p>
           <Link href="/contact">
             <button className="px-3 py-3 bg-orange-500 rounded-lg font-bold hover:bg-slate-300 hover:font-bold hover:text-orange-500">GET IN TOUCH</button>
           </Link>
